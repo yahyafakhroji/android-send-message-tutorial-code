ef0d2d88ae24283bc2ff624fb03b8a2603cf7656
kf6nvr <kf6nvr@gmail.com>
2010-08-17 18:14:11 +0000

    Repository tag for version code 1

--- src/com/mamlambo/tutorial/sendmessage/SendMessageActivity.java
+++ src/com/mamlambo/tutorial/sendmessage/SendMessageActivity.java
@@ -30,120 +30,58 @@
  */
 package com.mamlambo.tutorial.sendmessage;
 
-import java.io.File;
-
-
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 public class SendMessageActivity extends Activity {
 
-    private static final String LOG_TAG = "EmailLauncherActivity";
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	// Send a simple plain text email (no contents supplied)
+	public void sendSimpleEmail(View button) {
 
-    // Send a simple plain text email (no contents supplied)
-    public void sendSimpleEmail(View button) {
-        try {
-            Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-            emailIntent.setType("plain/text");
-            startActivity(emailIntent);
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "sendSimpleEmail() failed to start activity.", e);
-            Toast.makeText(this, "No handler", Toast.LENGTH_LONG).show();
-        }
-    }
+		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+		emailIntent.setType("plain/text");
+		startActivity(emailIntent);
+	}
 
-    public void sendPlainTextEmail(View button) {
-        try {
-            Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+	public void sendPlainTextEmail(View button) {
 
-            String aEmailList[] = { getResources().getString(
-                    R.string.email_address) };
-            String aEmailCCList[] = { getResources().getString(
-                    R.string.email_address_cc) };
-            String aEmailBCCList[] = { getResources().getString(
-                    R.string.email_address_bcc) };
+		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
 
-            emailIntent
-                    .putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
-            emailIntent.putExtra(android.content.Intent.EXTRA_CC, aEmailCCList);
-            emailIntent.putExtra(android.content.Intent.EXTRA_BCC,
-                    aEmailBCCList);
-            emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-                    getResources().getString(R.string.email_subject));
+		String aEmailList[] = { getResources()
+				.getString(R.string.email_address) };
+		String aEmailCCList[] = { getResources().getString(
+				R.string.email_address_cc) };
+		String aEmailBCCList[] = { getResources().getString(
+				R.string.email_address_bcc) };
 
-            emailIntent.setType("plain/text");
-            emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-                    getResources().getString(R.string.email_message));
+		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
+		emailIntent.putExtra(android.content.Intent.EXTRA_CC, aEmailCCList);
+		emailIntent.putExtra(android.content.Intent.EXTRA_BCC, aEmailBCCList);
+		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+				getResources().getString(R.string.email_subject));
 
-            startActivity(emailIntent);
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "sendPlainTextEmail() failed to start activity.", e);
-            Toast.makeText(this, "No handler", Toast.LENGTH_LONG).show();
-        }
-    }
+		emailIntent.setType("plain/text");
+		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, getResources()
+				.getString(R.string.email_message));
 
-    public void chooseEmail(View button) {
+		startActivity(emailIntent);
+	}
 
-        try {
-            Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-            emailIntent.setType("plain/text");
-            startActivity(Intent.createChooser(emailIntent,
-                    "Send your email in:"));
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "chooseEmail() failed to start activity.", e);
-            Toast.makeText(this, "No handler", Toast.LENGTH_LONG).show();
-        }
-    }
 
-    public void sendPictureMessage(View button) {
-        try {
-            Intent picMessageIntent = new Intent(
-                    android.content.Intent.ACTION_SEND);
-            picMessageIntent.setType("image/jpeg");
-            File downloadedPic = new File(
-                    Environment
-                            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                    "q.jpeg");
+	public void chooseEmail(View button) {
 
-            picMessageIntent.putExtra(Intent.EXTRA_STREAM, Uri
-                    .fromFile(downloadedPic));
+		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+		emailIntent.setType("plain/text");
+		startActivity(Intent.createChooser(emailIntent, getResources().getString(R.string.chooser)));
 
-            startActivity(Intent.createChooser(picMessageIntent, getResources()
-                    .getString(R.string.chooser_pic)));
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "sendPictureMessage() failed to start activity.", e);
-            Toast.makeText(this, "No handler", Toast.LENGTH_LONG).show();
-        }
-    }
-    
-    
-    public void launchForm(View button) {
-
-        Intent launchFormActivity = new Intent(this, FormActivity.class); 
-        startActivity(launchFormActivity);
-
-    }
-    
-    public void showInviteForm(View button) {
-        Intent launchInviteActivity = new Intent(this, InviteActivity.class);
-        startActivity(launchInviteActivity);
-    }
-    
-    public void gotoQuickContactBadgeScreen(View view) {
-        Intent launchQuickContactBadgeActivity = new Intent(this, QuickContactBadgeActivity.class);
-        startActivity(launchQuickContactBadgeActivity);
-    }
+	}
 }
